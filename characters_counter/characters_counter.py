5776589707cfdb0b429e66c6ff9f97cfc77f2453
Kristina Kulbych <k.kulbych@gmail.com>
2024-10-08 13:06:21 +0300

    remove if __name__ == '__main_'_ section in characters_counter/characters_counter.py

--- characters_counter/characters_counter.py
+++ characters_counter/characters_counter.py
@@ -13,13 +13,3 @@ def count_characters_occurrence(string: str) -> int:
         [1 for v in characters_occurrence.values() if v == 1]
     )
     return single_occurrence
-
-
-if __name__ == '__main__':
-    cases = [
-        ("abbbcccdf", 3),
-        ("aabbccdd", 0),
-        ("!@#123abcdee", 10),
-    ]
-    for input_string, expecter_result in cases:
-        assert count_characters_occurrence(input_string) == expecter_result
