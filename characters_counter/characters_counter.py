75fb4f4e0135c681161c540298d7719efe046913
Kristina Kulbych <k.kulbych@gmail.com>
2024-10-09 18:40:31 +0300

    add filter in  function count_characters_occurrence

--- characters_counter/characters_counter.py
+++ characters_counter/characters_counter.py
@@ -1,6 +1,8 @@
 from collections import Counter
 from functools import cache
 
+REQUIREMENT_OCCURRENCE = 1
+
 
 @cache
 def count_characters_occurrence(string: str) -> int:
@@ -8,8 +10,11 @@ def count_characters_occurrence(string: str) -> int:
     The function returns the number of characters in the string occuring only
     once.
     """
-    characters_occurrence = Counter(string)
-    single_occurrence = sum(
-        [1 for v in characters_occurrence.values() if v == 1]
-    )
-    return single_occurrence
+    characters_counts = Counter(string)
+    unique_occurrences = filter(
+                            lambda occurrence: (
+                                    occurrence == REQUIREMENT_OCCURRENCE
+                                        ), characters_counts.values()
+                            )
+    unique_characters_number = sum(unique_occurrences)
+    return unique_characters_number
