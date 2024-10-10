a04f3e14b9c8bd4912df994cc75f4e163cf7538a
Kristina Kulbych <k.kulbych@gmail.com>
2024-10-10 16:53:17 +0300

    add parametrize for test functions

--- tests/test_characters_counter.py
+++ tests/test_characters_counter.py
@@ -4,28 +4,21 @@ from characters_counter import count_characters_occurrence
 
 
 class TestCharactersCounter:
-    def test_count_characters_occurrence(self) -> None:
-        # Given
-        cases = [
-            ("abbbcccdf", 3),
-            ("aabbccdd", 0),
-            ("!@#123abcdee", 10),
-            ("", 0),
-            ("aAbBcC", 6),
-        ]
-        # When
-        for input_text, expected_result in cases:
-            # Then
-            assert count_characters_occurrence(input_text) == expected_result
+    @pytest.mark.parametrize(
+        'input_text, expected_result',
+        [("abbbcccdf", 3), ("aabbccdd", 0),
+         ("!@#123abcdee", 10), ("", 0),
+         ("aAbBcC", 6),]
+    )
+    def test_count_characters_occurrence(
+            self, input_text, expected_result) -> None:
+        assert count_characters_occurrence(input_text) == expected_result
 
-    def test_count_characters_occurrence_raises_expected_error_when_unhashable_input_is_provided(self) -> None:
-        # Given
-        cases = [123, 30.6, [], {}, set()]
-        # When
-        for input_text in cases:
-            # Then
-            with pytest.raises(TypeError):
-                count_characters_occurrence(input_text)
+    @pytest.mark.parametrize('input_text', [123, 30.6, [], {}, set()])
+    def test_count_characters_occurrence_raises_TypeError_when_unhashable_input_is_provided(
+            self, input_text) -> None:
+        with pytest.raises(TypeError):
+            count_characters_occurrence(input_text)
 
 
 if __name__ == '__main__':
