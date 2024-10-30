238f8be411314f995f921af2c6519e26d2e52bfd
Kristina Kulbych <k.kulbych@gmail.com>
2024-10-30 18:32:56 +0200

    correct comments in tests

--- tests/test_count_character_frequency.py
+++ tests/test_count_character_frequency.py
@@ -1,20 +1,21 @@
-from collections import Counter
 from pathlib import Path
 
 import pytest
 
-from characters_counter import count_character_frequency
+from characters_counter import CustomFileNotFoundError, count_character_frequency
 
 
 class TestCharacterFrequency:
 
     EXPECTED_RESULT = {
         "Typer is a library for building CLI applications that users will love using and developers will "
-        "love creating": Counter({"i": 20, "l": 20, "e": 16}),
-        "Based on Python type hints": Counter({"n": 3, "t": 3, "s": 2}),
-        "It's also a command line tool to run scripts, automatically converting them to CLI applications": Counter(
-            {"t": 10, "o": 9, "a": 8}
-        ),
+        "love creating": {"i": 10, "l": 10, "e": 8},
+        "Based on Python type hints": {"n": 3, "t": 3, "s": 2},
+        "It's also a command line tool to run scripts, automatically converting them to CLI applications": {
+            "t": 10,
+            "o": 9,
+            "a": 8,
+        },
         "": {},
     }
 
@@ -30,9 +31,8 @@ class TestCharacterFrequency:
         ],
     )
     def test_count_character_frequency_with_string_input(
-        self, data_input: Path, expected_result: dict[str, Counter[str]]
+        self, data_input: str, expected_result: dict[str, dict[str, int]]
     ) -> None:
-        # Given: data_input: Path
         # When
         actual_result = count_character_frequency(data_input)
         # Then
@@ -49,21 +49,22 @@ class TestCharacterFrequency:
         assert actual_result == expected_result
 
     def test_count_character_frequency_with_correct_filepath_input_using_textfile_sample(
-        self, existed_textfile: Path
+        self, existing_textfile: Path
     ) -> None:
         # Given
         expected_result = self.EXPECTED_RESULT
         # When
-        actual_result = count_character_frequency(existed_textfile)
+        actual_result = count_character_frequency(existing_textfile)
         # Then
         assert actual_result == expected_result
 
     def test_count_character_frequency_with_invalid_filepath(self) -> None:
         # Given
         invalid_filepath = Path("not_existed_file.txt")
-        # When: The count_character_frequency function is called
-        # Then
-        with pytest.raises(FileNotFoundError, match="Error! The file is not found or cannot be opened."):
+        # When / Then
+        with pytest.raises(
+            CustomFileNotFoundError, match=f"Error! The file {invalid_filepath} is not found or cannot be opened."
+        ):
             count_character_frequency(invalid_filepath)
 
 
