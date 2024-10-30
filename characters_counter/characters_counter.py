a2904f346b8735659cc3bb4a4e99ae0045e66797
Kristina Kulbych <k.kulbych@gmail.com>
2024-10-30 18:25:39 +0200

    add CustomFileNotFoundError

--- characters_counter/characters_counter.py
+++ characters_counter/characters_counter.py
@@ -1,10 +1,20 @@
 import re
-from collections import Counter, defaultdict
+from collections import Counter
 from functools import cache
 from pathlib import Path
 
 REQUIREMENT_OCCURRENCE = 1
 NUM_OF_MOST_COMMON_CHARACTERS = 3
+
+
+class CustomFileNotFoundError(Exception):
+    """
+    Raised when the file is not found or cannot be opened.
+    """
+
+    def __init__(self, filepath: Path):
+        super().__init__(f"Error! The file {filepath} is not found or cannot be opened.")
+        self.filepath = filepath
 
 
 @cache
@@ -18,42 +28,41 @@ def count_characters_occurrence(string: str) -> int:
     return unique_characters_number
 
 
-def get_frequent_characters_per_sentence(text: str) -> defaultdict[str, Counter[str]]:
+def get_frequent_characters_per_sentence(text: str) -> dict[str, dict[str, int]]:
     """
     The helper function counts the frequency of each character in the input sentence, excluding spaces.
     Single word is considered as a sentence as well.
     The number of characters returned is controlled by the global constant "NUM_OF_MOST_COMMON_CHARACTERS".
     """
     sentences = re.split(r"[.?!]", text)
-    character_frequency: defaultdict = defaultdict(Counter)
+    character_frequency: dict[str, dict[str, int]] = dict()
     for sentence in sentences:
         sentence = sentence.lstrip()
         count_characters = Counter(sentence.replace(" ", ""))
         frequent_characters_per_sentence = dict(count_characters.most_common(NUM_OF_MOST_COMMON_CHARACTERS))
-        character_frequency[sentence].update(Counter(frequent_characters_per_sentence))
+        character_frequency[sentence] = frequent_characters_per_sentence
     return character_frequency
 
 
 def open_and_read_textfile(filepath: Path) -> str:
     """
     The helper function attempts to open a text file and reads its contents as a string.
-    If the file is not found, a FileNotFoundError is raised.
+    If the file is not found, a CustomFileNotFoundError is raised.
     """
     try:
         with open(filepath, "r") as text_file:
-            text = text_file.read()
-            return text
-    except FileNotFoundError:
-        raise FileNotFoundError("Error! The file is not found or cannot be opened.")
+            return text_file.read()
+    except FileNotFoundError as error:
+        raise CustomFileNotFoundError(filepath) from error
 
 
-def count_character_frequency(data_input: Path) -> dict[str, Counter[str]]:
+def count_character_frequency(data_input: Path | str) -> dict[str, dict[str, int]]:
     """
     The function counts most used letters/symbols in each sentence from the given textfile.
     """
     if Path(data_input).suffix:
-        text = open_and_read_textfile(data_input)
+        text = open_and_read_textfile(Path(data_input))
     else:
         text = str(data_input)
     character_frequency = get_frequent_characters_per_sentence(text)
-    return dict(character_frequency)
+    return character_frequency
